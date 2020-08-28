9592e6511b7615c7519de98953beeffbed246728
luke-h1 <luke.howsam@yahoo.com>
2020-08-28 16:00:03 +0100

    fix error with multi api reqs, add err handling, fix some styles

--- app.js
+++ app.js
@@ -9,30 +9,38 @@ const errorHeading = document.getElementById('error-heading');
 function getDrinks(e) {
   e.preventDefault();
   const searchTerm = query.value;
-  const BASE_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`;
-  fetch(BASE_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      resultHeading.innerHTML = `<h2 class="search-results">Results for - ${searchTerm}</h2>`;
-      if (data.drinks === null) {
-        errorHeading.innerHTML = `No Search results for - ${searchTerm}`;
-      } else {
-        drinksEl.innerHTML = data.drinks
-          .map(
-            (drink) => `
-          <div class="drink"> 
-            <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" /> 
-            <div class="drink-information" data-drinkID="${drink.idDrink}"
-              <h3>${drink.strDrink}</h3> 
-            </div> 
-          </div> 
-          `
-          )
-          .join('');
-      }
-    });
-}
+  const API_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+  if (searchTerm.trim()) {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2 class="search-results">Results for - ${searchTerm}...</h2>`;
+        if (data.drinks === null) {
+          errorHeading.innerHTML = `No search results for - ${searchTerm}`;
+        } else if (data.drinks !== null) {
+          drinksEl.innerHTML = data.drinks
+            .map(
+              (drink) => `
+            <div class="drink"> 
+              <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" /> 
+              <div class="drink-information" data=drinkID="${drink.idDrink}" 
+              <h3>${drink.strDrink}</h2> 
+              </div> 
+              </div> 
+            `
+            )
+            .join(''); // turn arr to str
+        } 
+      });
+
+        } else { 
+          errorHeading.innerHTML = `<h2>Please enter a correct value 🤷‍♂️</h2>`;
+          window.setTimeout(() => {
+            errorHeading.innerHTML = '';
+          }, 2000);
+        }
+  }
 
 // EVENT LISTENERS
-submit.addEventListener('click', getDrinks);
+submit.addEventListener('submit', getDrinks);
