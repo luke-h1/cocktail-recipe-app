f751b66780023c8575fe39ad8d3ec3608eb03cc6
luke-h1 <luke.howsam@yahoo.com>
2020-08-28 16:23:59 +0100

    add random search functionality

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const submit = document.getElementById('submit');
 let drinksEl = document.getElementById('drinks');
 const resultHeading = document.getElementById('result-heading');
 const errorHeading = document.getElementById('error-heading');
-
+const randomDrinkBtn = document.getElementById('randomDrink');
+const singleDrinkEl = document.getElementById('single-drink');
 // search drinks and fetch from API
 
-function getDrinks(e) {
+async function getDrinks(e) {
   e.preventDefault();
   const searchTerm = query.value;
   const API_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`;
   if (searchTerm.trim()) {
-    fetch(API_URL)
+    await fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -31,16 +32,56 @@ function getDrinks(e) {
             `
             )
             .join(''); // turn arr to str
-        } 
-      });
-
-        } else { 
-          errorHeading.innerHTML = `<h2>Please enter a correct value 🤷‍♂️</h2>`;
-          window.setTimeout(() => {
-            errorHeading.innerHTML = '';
-          }, 2000);
         }
+      });
+  } else {
+    errorHeading.innerHTML = `<h2>Please enter a correct value 🤷‍♂️</h2>`;
+    window.setTimeout(() => {
+      errorHeading.innerHTML = '';
+    }, 2000);
   }
+}
+
+function addDrinksToPage(drink) {
+  const recipe = [];
+  for (let i = 1; i <= 20; i++) {
+    if (drink[`strIngredient${i}`]) {
+      recipe.push(`${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+  singleDrinkEl.innerHTML = `
+    <div class="single-drink"> 
+      <h1>${drink.idDrink}</h1> 
+      <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" /> 
+      <div class="single-drink-info"> 
+      ${drink.strCategory ? `<p>${drink.strCategory}</p>` : ''} 
+      ${drink.strAlcoholic ? `<p>${drink.strAlcoholic}</p>` : ''} 
+      </div> 
+      <div class="main"> 
+      <p>${drink.strInstructions}</p>  
+      <h2>Ingredients: </h2> 
+      <ul> 
+      ${recipe.map((item) => `<li>${item}</li>`).join('')}
+      </ul> 
+      </div> 
+      </div> 
+  `;
+}
+
+function getRandomDrink() {
+  drinksEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+  const randomDrinkURL = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+  fetch(randomDrinkURL)
+    .then((res) => res.json())
+    .then((data) => {
+      const drink = data.drinks[0];
+      addDrinksToPage(drink);
+    });
+}
 
 // EVENT LISTENERS
 submit.addEventListener('submit', getDrinks);
+randomDrinkBtn.addEventListener('click', getRandomDrink);
