594d787119b3b0d8e4566749ee0ca38c08350215
luke-h1 <luke.howsam@yahoo.com>
2020-10-17 20:34:20 +0100

    add Loading.js

---
+++ src/components/Layout/Loading.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const Loading = () => {
+  return (
+    <Fragment>
+      <img
+        src="https://media.giphy.com/media/3AMRa6DRUhMli/giphy.gif"
+        alt="loading..."
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </Fragment>
+  );
+};
+export default Loading;
