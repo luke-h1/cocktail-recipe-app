1f66d80693d9660e546a6b5b69d9177e66f51698
luke-h1 <luke.howsam@yahoo.com>
2020-10-26 21:08:10 +0000

    updates

--- src/components/DrinkItem/DrinkItem.js
+++ src/components/DrinkItem/DrinkItem.js
@@ -1,20 +1,28 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import './DrinkItem.scss'
+import './DrinkItem.scss';
 
 const DrinkItem = (props) => {
   return (
     <Fragment>
       <div className="cards__item">
         <Link className="cards__item__link" to={`/${props.id}`}>
-          <figure className="cards__item__pic-wrap" data-category={props.drink.strDrink}>
-            <img src={props.drink.strDrinkThumb} alt="Travel" className="cards__item__img" />
+          <figure
+            className="cards__item__pic-wrap"
+            data-category={props.drink.strCategory}
+          >
+            <img
+              src={props.drink.strDrinkThumb}
+              alt="Travel"
+              className="cards__item__img"
+            />
           </figure>
           <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.drink.strDrink}</h5>
           </div>
         </Link>
       </div>
     </Fragment>
   );
 };
-export default DrinkItem
+export default DrinkItem;
