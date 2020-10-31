7e3027c9c890648751e9199ed539cf3007a3cf1a
luke-h1 <luke.howsam@yahoo.com>
2020-10-31 20:19:43 +0000

    add class

--- src/components/DrinkItem/DrinkItem.js
+++ src/components/DrinkItem/DrinkItem.js
@@ -6,22 +6,22 @@ const DrinkItem = (props) => {
   return (
     <Fragment>
       <div className="cards__item">
-        {/* <Link className="cards__item__link" to="/drink/:id"> */}{' '}
-        {/* TODO: ROUTE TO SINGLE DRINK */}
-        <figure
-          className="cards__item__pic-wrap"
-          data-category={props.drink.strCategory}
-        >
-          <img
-            src={props.drink.strDrinkThumb}
-            alt="Travel"
-            className="cards__item__img"
-          />
-        </figure>
-        <div className="cards__item__info">
-          <h5 className="cards__item__text">{props.drink.strDrink}</h5>
-        </div>
-        {/* </Link> */}
+        <Link className="cards__item__link" to="">
+          {' '}
+          <figure
+            className="cards__item__pic-wrap"
+            data-category={props.drink.strCategory}
+          >
+            <img
+              src={props.drink.strDrinkThumb}
+              alt="Travel"
+              className="cards__item__img"
+            />
+          </figure>
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.drink.strDrink}</h5>
+          </div>
+        </Link>
       </div>
     </Fragment>
   );
