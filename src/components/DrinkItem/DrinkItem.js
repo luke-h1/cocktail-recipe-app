7843ceafd74927d1689612be2eba48921044cc66
luke-h1 <luke.howsam@yahoo.com>
2020-11-02 18:34:33 +0000

    refactor for drinkItem to map over ingredients and measurements

--- src/components/DrinkItem/DrinkItem.js
+++ src/components/DrinkItem/DrinkItem.js
@@ -1,8 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment,useState } from 'react';
 import { Link } from 'react-router-dom';
 import './DrinkItem.scss';
 
-const DrinkItem = (props) => {
+function DrinkItem(props) {
+  const [item, setItem] = useState([]);
+
+  const addDrinksToPage = (drink) => {
+    const recipe = [];
+    for (let i = 1; i <= 20; i++) {
+      if (props.drink[`strIngredients${i}`]) {
+        recipe.push(
+          `${drink[`strIngredients${i}`]} - ${drink[`strMeasures${i}`]}`
+        );
+        setItem(recipe);
+      } else {
+        return;
+      }
+    }
+  };
+
   return (
     <Fragment>
       <div className="cards__item">
@@ -22,10 +38,12 @@ const DrinkItem = (props) => {
             <h5 className="cards__item__text">{props.drink.strDrink}</h5>
             <p className="cards__item-recipe">Measurements & recipe:</p>
             <p className="test">
-              {props.drink.strIngredient1 ? props.drink.strIngredient1 : null} -{' '}
+              {}
+              {props.drink.strIngredient1
+                ? props.drink.strIngredient1
+                : null} -{' '}
               {props.drink.strMeasure1 ? props.drink.strMeasure1 : null}
             </p>
-
 
             <p className="test">{props.drink.strInstructions}</p>
           </div>
@@ -33,5 +51,5 @@ const DrinkItem = (props) => {
       </div>
     </Fragment>
   );
-};
+}
 export default DrinkItem;
